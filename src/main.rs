90fde935ca9d0636d7ff8e9eb7bd49b1681534ff
Aaron Gaut <git@aarongaut.com>
2022-03-24 20:13:24 -0700

    Added basic graphics and ability to move using keyboard input (milestone 1)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::{
     core::FixedTimestep,
     prelude::*,
+    app::{AppExit},
 };
 
 const TIME_STEP: f32 = 1.0 / 60.0;
@@ -9,29 +10,69 @@ const TIME_STEP: f32 = 1.0 / 60.0;
 struct Label(String);
 
 #[derive(Component)]
-struct Transform(Vec3);
+struct Velocity(Vec3);
 
 #[derive(Component)]
-struct Velocity(Vec3);
+struct Player;
 
 fn physics_system(mut query: Query<(&mut Transform, &Velocity)>) {
     for (mut transform, velocity) in query.iter_mut() {
-        transform.0 += velocity.0 * TIME_STEP;
+        transform.translation += velocity.0 * TIME_STEP;
     }
+}
+
+fn input_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Velocity, &Player)>,
+    mut app_exit_events: EventWriter<AppExit>,
+) {
+    let mut velocity = query.single_mut().0;
+    let mut direction = Vec3::ZERO;
+    if keyboard_input.pressed(KeyCode::W) {
+        direction.y += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        direction.x -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        direction.y -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        direction.x += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::Escape) {
+        app_exit_events.send(AppExit);
+    }
+    direction = direction.normalize_or_zero();
+    velocity.0 = 50.0 * direction;
 }
 
 fn debug_system(query: Query<(&Label, &Transform)>) {
     for (label, transform) in query.iter() {
-        println!("{}: {}", label.0, transform.0);
+        println!("{}: {}", label.0, transform.translation);
     }
 }
 
 fn startup_system(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
     commands
         .spawn()
         .insert(Label("Test point 1".to_string()))
-        .insert(Transform(Vec3::new(0.0, 0.0, 0.0)))
-        .insert(Velocity(Vec3::new(0.1, 0.0, 0.0)));
+        .insert_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: Vec3::new(5.0, 5.0, 1.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::rgb(0.0, 1.0, 0.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Velocity(Vec3::ZERO))
+        .insert(Player);
 }
 
 fn main() {
@@ -41,6 +82,7 @@ fn main() {
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
+                .with_system(input_system)
                 .with_system(physics_system)
         )
         .add_system_set(
